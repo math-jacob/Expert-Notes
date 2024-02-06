1fa51b6df0970d3346abca4c60d55aa1174bb2ae
matheus-jacob <math.henrique.jacob@gmail.com>
2024-02-06 10:24:13 -0300

    add favicon and logo

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
+import logo from "./assets/logo-nlw-expert.svg"
+
 export function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <img src={logo} />
   )
 }
