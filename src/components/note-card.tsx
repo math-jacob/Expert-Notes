f874760a91cd2aee75753703931acd4008e9ac94
matheus-jacob <math.henrique.jacob@gmail.com>
2024-02-06 14:16:22 -0300

    changed card container to button instead of div

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,6 +1,6 @@
 export function NoteCard() {
   return (
-    <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
+    <button className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
       <span className="text-sm font-medium text-slate-300">
         Há 2 dias
       </span>
@@ -10,6 +10,6 @@ export function NoteCard() {
       </p>
 
       <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-  </div>
+    </button>
   )
 }
