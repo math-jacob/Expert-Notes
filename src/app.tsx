b833f5b708c0951759b83c3c27a2023892c2ab89
matheus-jacob <math.henrique.jacob@gmail.com>
2024-02-06 14:09:01 -0300

    add note card component

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import logo from "./assets/logo-nlw-expert.svg"
+import { NoteCard } from "./components/note-card"
 
 export function App() {
   return (
@@ -25,39 +26,9 @@ export function App() {
           </p>
         </div>
 
-        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
-          <span className="text-sm font-medium text-slate-300">
-            Há 2 dias
-          </span>
-          <p className="text-sm leading-6 text-slate-400">
-           Lorem ipsum dolor sit amet consectetur adipisicing elit. At laborum, nesciunt sed sunt facere eligendi, magni reprehenderit iusto cum consequatur libero aut autem sequi temporibus dignissimos praesentium tenetur officiis! Quam?
-           Lorem ipsum dolor sit amet consectetur adipisicing elit. At laborum, nesciunt sed sunt facere eligendi, magni reprehenderit iusto cum consequatur libero aut autem sequi temporibus dignissimos praesentium tenetur officiis! Quam?
-          </p>
-
-          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-        </div>
-
-        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
-          <span className="text-sm font-medium text-slate-300">
-            Há 4 dias
-          </span>
-          <p className="text-sm leading-6 text-slate-400">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque dolore quasi optio laudantium. Dicta, cupiditate molestiae! Dolores ipsam repudiandae dicta ducimus officiis dignissimos! Asperiores iste facilis aut impedit doloribus repellendus.
-          </p>
-
-          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-        </div>
-
-        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
-          <span className="text-sm font-medium text-slate-300">
-            Há 7 dias
-          </span>
-          <p className="text-sm leading-6 text-slate-400">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque dolore quasi optio laudantium. Dicta, cupiditate molestiae! Dolores ipsam repudiandae dicta ducimus officiis dignissimos! Asperiores iste facilis aut impedit doloribus repellendus.
-          </p>
-
-          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-        </div>
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
       </div>
     </div>
   )
