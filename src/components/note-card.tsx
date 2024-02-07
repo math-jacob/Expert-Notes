64a0d15aaef6d7951c496894a4421551731e5563
matheus-jacob <math.henrique.jacob@gmail.com>
2024-02-07 19:29:09 -0300

    add delete note button

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -25,7 +25,7 @@ export function NoteCard({ note }: NoteCardProps) {
 
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/50" />
-        <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+        <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
           <div className="flex flex-1 flex-col gap-3 p-5">
             <span className="text-sm font-medium text-slate-300">
               {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
@@ -34,6 +34,13 @@ export function NoteCard({ note }: NoteCardProps) {
               {note.content}
             </p>
           </div>
+
+          <button 
+            type="button"
+            className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"
+          >
+            Deseja <span className="text-red-400 group-hover:underline">apagar essa nota</span>?
+          </button>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
