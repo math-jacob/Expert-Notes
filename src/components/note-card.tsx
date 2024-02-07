5655d06f7586e1e391a175e9e943e06b9e0a3a37
matheus-jacob <math.henrique.jacob@gmail.com>
2024-02-07 18:35:09 -0300

    add props to note card

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,12 +1,18 @@
-export function NoteCard() {
+interface NoteCardProps {
+  note: {
+    date: Date
+    content: string
+  }
+}
+
+export function NoteCard({ note }: NoteCardProps) {
   return (
-    <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden outline-none relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+    <button className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden outline-none relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
       <span className="text-sm font-medium text-slate-300">
-        Há 2 dias
+        {note.date.toISOString()}
       </span>
       <p className="text-sm leading-6 text-slate-400">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. At laborum, nesciunt sed sunt facere eligendi, magni reprehenderit iusto cum consequatur libero aut autem sequi temporibus dignissimos praesentium tenetur officiis! Quam?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. At laborum, nesciunt sed sunt facere eligendi, magni reprehenderit iusto cum consequatur libero aut autem sequi temporibus dignissimos praesentium tenetur officiis! Quam?
+        {note.content}
       </p>
 
       <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
