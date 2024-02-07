fac9364c1f1e6083810e66630fbc6ccb3a106d58
matheus-jacob <math.henrique.jacob@gmail.com>
2024-02-07 20:50:54 -0300

    add form on Add Note Card for semantic reasons

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -33,34 +33,36 @@ export function NewNoteCard() {
             <X className="size-5" />
           </Dialog.Close>
 
-          <div className="flex flex-1 flex-col gap-3 p-5">
-            <span className="text-sm font-medium text-slate-300">
-              Adicionar nota
-            </span>
+          <form>
+            <div className="flex flex-1 flex-col gap-3 p-5">
+              <span className="text-sm font-medium text-slate-300">
+                Adicionar nota
+              </span>
 
-            {shouldShowOnboarding ? 
-              (
-                <p className="text-sm leading-6 text-slate-400">
-                  Comece <button className="font-medium text-lime-400 hover:underline">gravando uma nota</button> em áudio ou se preferir <button onClick={handleStartEditor} className="font-medium text-lime-400 hover:underline">utilize apenas texto</button>.
-                </p>
-              ) 
-              : 
-              (
-                <textarea 
-                  autoFocus 
-                  className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
-                  onChange={handleContentChanged}
-                />
-              )
-            }
-          </div>
+              {shouldShowOnboarding ? 
+                (
+                  <p className="text-sm leading-6 text-slate-400">
+                    Comece <button className="font-medium text-lime-400 hover:underline">gravando uma nota</button> em áudio ou se preferir <button onClick={handleStartEditor} className="font-medium text-lime-400 hover:underline">utilize apenas texto</button>.
+                  </p>
+                ) 
+                : 
+                (
+                  <textarea 
+                    autoFocus 
+                    className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
+                    onChange={handleContentChanged}
+                  />
+                )
+              }
+            </div>
 
-          <button 
-            type="button"
-            className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
-          >
-            Salvar nota
-          </button>
+            <button 
+              type="button"
+              className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
+            >
+              Salvar nota
+            </button>
+          </form>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
