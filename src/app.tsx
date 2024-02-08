5ab9751811c5e2bca7198a5de5b5286c555f0217
matheus-jacob <math.henrique.jacob@gmail.com>
2024-02-08 09:14:28 -0300

    add notes state on app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,14 @@
+import { useState } from "react"
 import logo from "./assets/logo-nlw-expert.svg"
 import { NewNoteCard } from "./components/new-note-card"
 import { NoteCard } from "./components/note-card"
 
 export function App() {
+  const [notes, setNotes] = useState([
+    {id: 1, date: new Date(), content: "Hello World"},
+    {id: 2, date: new Date(), content: "Nota 2"}
+  ])
+
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt="NLW Expert"/>
@@ -20,10 +26,9 @@ export function App() {
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
 
-        <NoteCard note={{
-          date: new Date(),
-          content: "Hello World"
-        }} />
+        {notes.map(note => {
+          return <NoteCard key={note.id} note={note} />
+        })}
       </div>
     </div>
   )
