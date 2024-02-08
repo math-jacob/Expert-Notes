182851f782aae72807dad26cc22053f423dda070
matheus-jacob <math.henrique.jacob@gmail.com>
2024-02-08 10:11:35 -0300

    add input id to fix browser warning

--- src/app.tsx
+++ src/app.tsx
@@ -50,7 +50,8 @@ export function App() {
       <img src={logo} alt="NLW Expert"/>
 
       <form className="w-full">
-        <input 
+        <input
+          id="search"
           type="text"
           placeholder="Busque em suas notas..."
           className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
