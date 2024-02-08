8f8cac4d2848e50a74424165249b799d0f163d21
matheus-jacob <math.henrique.jacob@gmail.com>
2024-02-08 11:40:53 -0300

    add delete notes funcionality

--- src/app.tsx
+++ src/app.tsx
@@ -35,6 +35,17 @@ export function App() {
     localStorage.setItem("notes", JSON.stringify(notesArray))
   }
 
+  function onNoteDeleted(id: string) {
+    const notesArray = notes.filter(note => {
+      return note.id !== id
+    })
+
+    console.log(notesArray)
+
+    setNotes(notesArray)
+    localStorage.setItem("notes", JSON.stringify(notesArray))
+  }
+
   function handleSearch(event: ChangeEvent<HTMLInputElement>) {
     const query = event.target.value
 
@@ -65,7 +76,7 @@ export function App() {
         <NewNoteCard onNoteCreated={onNoteCreated} />
 
         {filteredNotes?.map(note => {
-          return <NoteCard key={note.id} note={note} />
+          return <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted}/>
         })}
       </div>
     </div>
